fadc5a3f951ad3014749ccb340f0e44f83475168
Alex M <fmirc21@student.aau.dk>
2022-10-26 09:49:07 +0200

    Revised noise reduction code

--- NoiseReduction.py
+++ NoiseReduction.py
@@ -8,34 +8,35 @@ inputPic = cv.imread("Resources/hamsta input.jpg")
 inputPic_gray = cv.cvtColor(inputPic, cv.COLOR_BGR2GRAY)
 inputPic_arr = np.array(inputPic_gray)
 
-def median_filter(data, filter_size):
+
+def median_filter(data, kernel):
     temp = []
-    indexer = filter_size // 2
-    data_final = []
-    data_final = np.zeros(((len(data), len(data[0]))), dtype=np.uint8)
+    indexer = kernel // 2
+    window = [
+        (i, j)
+        for i in range(-indexer, kernel - indexer)
+        for j in range(-indexer, kernel - indexer)
+    ]
+    index = len(window) // 2
     for i in range(len(data)):
-
         for j in range(len(data[0])):
+            data[i][j] = sorted(
+                0 if (
+                        min(i + a, j + b) < 0
+                        or len(data) <= i + a
+                        or len(data[0]) <= j + b
+                ) else data[i + a][j + b]
+                for a, b in window
+            )[index]
+    return data
 
-            for z in range(filter_size):
-                if i + z - indexer < 0 or i + z - indexer > len(data) - 1:
-                    for c in range(filter_size):
-                        temp.append(0)
-                else:
-                    if j + z - indexer < 0 or j + indexer > len(data[0]) - 1:
-                        temp.append(0)
-                    else:
-                        for k in range(filter_size):
-                            temp.append(data[i + z - indexer][j + k - indexer])
-
-            temp.sort()
-            data_final[i][j] = temp[len(temp) // 2]
-            temp = []
-    return data_final
 
 filtered_img = median_filter(inputPic_arr, 3)
-
 
 cv.imshow("Filtered Image", filtered_img)
 cv.imshow("Input Image", inputPic)
 cv.waitKey(0)
+
+# slice = data[ i - filter_size : i + filter_size]
+
+# slice = slice.flatten
