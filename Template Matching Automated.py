b324a2686acb6b3855a1063bceaf93c4ae820bc7
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2022-10-26 10:08:33 +0200

    Inbuilt OpenCV Template Matching Function

--- Template Matching Automated.py
+++ Template Matching Automated.py
@@ -2,26 +2,27 @@ import cv2 as cv
 import numpy as np
 
 #read image
-inputPic = cv.imread("Resources/1.jpg")
+inputPic = cv.imread("Resources/input picture.jpg")
 
 #convert the image
 input_gray = cv.cvtColor(inputPic, cv.COLOR_BGR2GRAY)
 
-#template matching to find crowns
-template = cv.imread("Resources/templateRAW.png", 0)
+templateTest = cv.imread("Output/CroppedPicture.jpg", 0)
 
-w, h = template.shape[::-1]
+def TemplateMatching(image, template):
+    w, h = template.shape[::-1]
 
-res = cv.matchTemplate(input_gray, template, cv.TM_CCOEFF_NORMED)
-threshold = 0.65
-loc = np.where(res >= threshold)
+    res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
+    threshold = 0.9
+    loc = np.where(res >= threshold)
 
+    for pt in zip(*loc[::-1]):
+        cv.rectangle(inputPic, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-for pt in zip(*loc[::-1]):
-    cv.rectangle(inputPic, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+    #showing the result
+    # cv.imshow("Correlation", res)
+    cv.imwrite('Output/TemplateMatched.png', inputPic)
+    cv.imshow("Result", inputPic)
+    cv.waitKey(0)
 
-#showing the result
-cv.imwrite('res.png', inputPic)
-cv.imshow("Correlation", res)
-cv.imshow("Input Image", inputPic)
-cv.waitKey(0)
+TemplateMatching(input_gray, templateTest)
