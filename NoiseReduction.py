efa3fac99493354a5fcbd19c6977741cd9323d2f
Markus <xp73vc@student.aau.dk>
2022-10-31 10:38:02 +0100

    Gaussian filter

--- NoiseReduction.py
+++ NoiseReduction.py
@@ -2,6 +2,7 @@
 
 import cv2 as cv
 import numpy as np
+import math
 from PIL import Image
 
 #inputPic = cv.imread("Resources/hamsta input.jpg")
@@ -41,3 +42,31 @@ def median_filter(data, kernel):
 # slice = data[ i - filter_size : i + filter_size]
 
 # slice = slice.flatten
+
+
+# Gaussian filter code from lecture 4
+def convolve(image, kernel):
+    kernel_size = kernel.shape[0]
+    output = np.zeros((img.shape[0] - kernel_size + 1, img.shape[1] - kernel_size + 1), dtype=np.uint8)
+    for y in range(output.shape[0]):
+        for x in range(output.shape[1]):
+            slice = image[y:y+kernel_size, x:x+kernel_size]
+            output[y, x] = np.sum(slice*kernel)/np.sum(kernel)
+    return output
+
+def generate_gaussian_kernel(radius, standard_deviation):
+    gaussian = [(1/(standard_deviation*math.sqrt(2*math.pi)))*math.exp(-0.5*(x/standard_deviation)**2) for x in range(-radius, radius+1)]
+    gaussian_kernel = np.zeros((radius*2+1,radius*2+1))
+    for y in range(gaussian_kernel.shape[0]):
+        for x in range(gaussian_kernel.shape[1]):
+            gaussian_kernel[y, x] = gaussian[y] * gaussian[x]
+    gaussian_kernel *= 1/gaussian_kernel[0,0]
+    return gaussian_kernel
+
+img = cv.imread("lion.jpg", cv.IMREAD_GRAYSCALE)
+
+mean_kernel = np.ones((11,11))
+mean = convolve(img, mean_kernel)
+
+gaussian_kernel = generate_gaussian_kernel(4, 2.2)
+gaussian = convolve(img, gaussian_kernel)
