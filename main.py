c36613cf69c7258b0ea9b26718e03d2f87209544
Jacob-h-v <jvillu21@student.aau.dk>
2022-11-04 11:48:48 +0100

    mainMain updated againAgain

--- main.py
+++ main.py
@@ -9,24 +9,31 @@ from Morphology import inbuiltMorphology, OpType
 imageInput = cv.imread("Resources/1M-2L-1P-1CL-1C (1).png")
 imageInput_gray = cv.cvtColor(imageInput, cv.COLOR_BGR2GRAY)
 imageInput_arr = np.array(imageInput_gray)
+gaussian_radius = 0
 
 tempCoords = True
 medianFilter = True
+
 convolve_with_gaussian = True
+# Hint: Don't touch these two :D
 adjustImgSize = True
 subtractBlurred = True
+
 binaryThresh = True
 closing = True
 createTemplate = True
 matchTemplates = True
 
+# Grab temmplate coordinates
 if tempCoords:
     template_coords = template_cropping(imageInput)
 
+# Run median filter
 if medianFilter:
     image_processed = median_filter(imageInput_gray, 3)
 
-if convolve_with_gaussian:
+# convolve with gaussian happens here
+if convolve_with_gaussian & medianFilter:
     gaussian_radius = 25
     gaussian_kernel = generate_gaussian_kernel(gaussian_radius, 500)
     image_blurred = convolve(image_processed, gaussian_kernel)
@@ -36,23 +43,73 @@ if convolve_with_gaussian:
         image_resize = cv.resize(image_processed, new_image_size, interpolation=cv.INTER_LINEAR)
         if subtractBlurred:
             image_subtracted = cv.subtract(image_resize, image_blurred)
+elif convolve_with_gaussian:
+    gaussian_radius = 25
+    gaussian_kernel = generate_gaussian_kernel(gaussian_radius, 500)
+    image_blurred = convolve(imageInput_gray, gaussian_kernel)
+    if adjustImgSize:
+        new_image_size = (
+        imageInput_gray.shape[1] - gaussian_radius * 2, imageInput_gray.shape[0] - gaussian_radius * 2)
+        image_resize = cv.resize(imageInput_gray, new_image_size, interpolation=cv.INTER_LINEAR)
+        if subtractBlurred:
+            image_subtracted = cv.subtract(image_resize, image_blurred)
+
+# Binary thresholding happens here
+if binaryThresh & convolve_with_gaussian:
+    ret, image_binary = cv.threshold(image_subtracted, 25, 255, cv.THRESH_BINARY)
+elif binaryThresh & medianFilter:
+    ret, image_binary = cv.threshold(image_processed, 25, 255, cv.THRESH_BINARY)
+elif binaryThresh:
+    ret, image_binary = cv.threshold(imageInput_gray, 25, 255, cv.THRESH_BINARY)
+
+# Run closing operation
+if closing & binaryThresh:
+    image_closed = inbuiltMorphology(image_binary, 5, OpType.Closing)
+elif closing & convolve_with_gaussian:
+    image_closed = inbuiltMorphology(image_subtracted, 5, OpType.Closing)
+elif closing & medianFilter:
+    image_closed = inbuiltMorphology(image_processed, 5, OpType.Closing)
+elif closing:
+    image_closed = inbuiltMorphology(imageInput_gray, 5, OpType.Closing)
+
+# Crop out the selected template using coordinates
+if createTemplate & closing & tempCoords:
+    template = crop(image_closed, template_coords[0] - gaussian_radius, template_coords[1] - gaussian_radius, template_coords[2] - gaussian_radius, template_coords[3] - gaussian_radius)
+elif createTemplate & binaryThresh & tempCoords:
+    template = crop(image_binary, template_coords[0] - gaussian_radius, template_coords[1] - gaussian_radius, template_coords[2] - gaussian_radius, template_coords[3] - gaussian_radius)
+elif createTemplate & convolve_with_gaussian & tempCoords:
+    template = crop(image_subtracted, template_coords[0] - gaussian_radius, template_coords[1] - gaussian_radius, template_coords[2] - gaussian_radius, template_coords[3] - gaussian_radius)
+elif createTemplate & tempCoords & medianFilter:
+    template = crop(image_processed, template_coords[0] - gaussian_radius, template_coords[1] - gaussian_radius, template_coords[2] - gaussian_radius, template_coords[3] - gaussian_radius)
+elif createTemplate & tempCoords:
+    template = crop(imageInput_gray, template_coords[0] - gaussian_radius, template_coords[1] - gaussian_radius, template_coords[2] - gaussian_radius, template_coords[3] - gaussian_radius)
+
+
+
+
+
+if matchTemplates & createTemplate & closing & tempCoords:
+    templateMatching_result = TemplateMatching(imageInput, image_closed, template, gaussian_radius)
+elif matchTemplates & createTemplate & binaryThresh & tempCoords:
+    templateMatching_result = TemplateMatching(imageInput, image_binary, template, gaussian_radius)
+elif matchTemplates & createTemplate & convolve_with_gaussian & tempCoords:
+    templateMatching_result = TemplateMatching(imageInput, image_subtracted, template, gaussian_radius)
+elif matchTemplates & createTemplate & medianFilter & tempCoords:
+    templateMatching_result = TemplateMatching(imageInput, image_processed, template, gaussian_radius)
+elif matchTemplates & createTemplate & tempCoords:
+    templateMatching_result = TemplateMatching(imageInput, imageInput_gray, template, gaussian_radius)
+    
+if convolve_with_gaussian:
+    cv.imshow("blur", image_blurred)
+    cv.imshow("Subtracted", image_subtracted)
 
 if binaryThresh:
-    ret, image_binary = cv.threshold(image_subtracted, 25, 255, cv.THRESH_BINARY)
+    cv.imshow("Binary", image_binary)
 
-if closing:
-    image_closed = inbuiltMorphology(image_binary, 5, OpType.Closing)
+if createTemplate & tempCoords:
+    cv.imshow("template", template)
 
-if createTemplate:
-    template = crop(image_closed, template_coords[0] - gaussian_radius, template_coords[1] - gaussian_radius, template_coords[2] - gaussian_radius, template_coords[3] - gaussian_radius)
+if createTemplate & matchTemplates & tempCoords:
+    cv.imshow("template matching", templateMatching_result)
 
-if matchTemplates:
-    templateMatching_result = TemplateMatching(imageInput, image_closed, template, gaussian_radius)
-    
-
-cv.imshow("blur", image_blurred)
-cv.imshow("Subtracted", image_subtracted)
-cv.imshow("Binary", image_binary)
-cv.imshow("template", template)
-cv.imshow("template matching", templateMatching_result)
 cv.waitKey(0)
