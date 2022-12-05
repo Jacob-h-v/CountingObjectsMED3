1278b5e964b833a88abd58236e41180982777722
Jacob-h-v <jvillu21@student.aau.dk>
2022-12-05 15:17:25 +0100

    Added FindBlobber.py. Still empty as of right now. Still need to translate source form java to python

---
+++ FindBlobber.py
@@ -0,0 +1,6 @@
+# Source:
+# Daniel Shiffman
+# http://codingtra.in
+# http://patreon.com/codingtrain
+# Code for: https://youtu.be/1scFcY-xMrI
+# https://github.com/CodingTrain/website-archive/blob/main/Tutorials/Processing/11_video/sketch_11_8_BlobTracking_improved/sketch_11_8_BlobTracking_improved.pde
